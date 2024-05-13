9d37bf4f856ada3644fce1c7d8fcd0894795b15c
CaptainRedtop <134284651+CaptainRedtop@users.noreply.github.com>
2024-05-13 20:46:11 +0200

    ThreadPool

---
+++ ThreadPool/ThreadPool/Torturial.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadPool
+{
+    internal class Torturial
+    {
+        internal void Main()
+        {
+            Stopwatch mywatch = new Stopwatch();
+            Console.WriteLine("Thread Pool Execution");
+            mywatch.Start();
+            ProcessWithThreadPoolMethod();
+            mywatch.Stop();
+            Console.WriteLine("Time consumed by ProcessWithThreadPoolMethod is : " + mywatch.ElapsedTicks.ToString());
+            mywatch.Reset();
+            Console.WriteLine("Thread Execution");
+            mywatch.Start();
+            ProcessWithThreadMethod();
+            mywatch.Stop();
+            Console.WriteLine("Time consumed by ProcessWithThreadMethod is : " + mywatch.ElapsedTicks.ToString());
+            Console.ReadLine();
+        }
+
+        internal void ProcessWithThreadPoolMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                System.Threading.ThreadPool.QueueUserWorkItem(Process);
+            }
+        }
+        internal void ProcessWithThreadMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Thread obj = new Thread(Process);
+                obj.Start();
+            }
+        }
+        internal void Process(object callback)
+        {
+        }
+    }
+
+}
